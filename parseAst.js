c481caefed200b19720ab5a94506bb3336aa0e07
Dhivyashree-1227 <dhivyabommi123@gmail.com>
2025-10-25 13:42:52 +0530

    frontend\node_modules\rollup\dist\es

--- parseAst.js
+++ parseAst.js
@@ -7,16 +7,6 @@
 
 	Released under the MIT License.
 */
-'use strict';
-
-Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-
-require('./native.js');
-const parseAst_js = require('./shared/parseAst.js');
-require('node:path');
-
-
-
-exports.parseAst = parseAst_js.parseAst;
-exports.parseAstAsync = parseAst_js.parseAstAsync;
-//# sourceMappingURL=parseAst.js.map
+import '../native.js';
+export { parseAst, parseAstAsync } from './shared/parseAst.js';
+import 'node:path';
